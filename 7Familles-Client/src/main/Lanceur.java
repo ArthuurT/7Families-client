9deb21d90c6e76bca01cea46c1a3f3d3b80e3805
Lastshot97 <juju7397@gmail.com>
2019-10-16 14:15:38 +0200

    Configuration file

--- 7Familles-Client/src/main/Lanceur.java
+++ 7Familles-Client/src/main/Lanceur.java
@@ -1,27 +1,46 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Properties;
 
 import remote.IServeur;
 
 public class Lanceur {
 
 	public static void main(String[] args) {
-		int port = Integer.parseInt(args[0]);
-		System.setProperty(
-			"java.security.policy",
-			"file:///home/asao/Documents/Universite/Semestre9/Middlewares/7Families/7Families-client/7Familles-Client/src/security.policy");
-		if (System.getSecurityManager() == null) {
-		    System.setSecurityManager(new SecurityManager());
-		}
 		try {
+			String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+			String configPath = rootPath + "config.properties";
+			
+			Properties properties = new Properties();
+			properties.load(new FileInputStream(configPath));
+			
+			String securityPolicyPath = "file:" + properties.getProperty("security.file.path");
+			System.setProperty("java.security.policy", securityPolicyPath);
+			if (System.getSecurityManager() == null) {
+			    System.setSecurityManager(new SecurityManager());
+			}
+			
+			int port = Integer.parseInt(properties.getProperty("server.port"));
+			String serverInterface = properties.getProperty("server.interface");
+			
 			Registry registry = LocateRegistry.getRegistry(port);
-			IServeur serveur = (IServeur) registry.lookup("serveur");
+			IServeur serveur = (IServeur) registry.lookup(serverInterface);
 			new ThreadJoueur(serveur).start();
+			
 		} catch (RemoteException | NotBoundException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
